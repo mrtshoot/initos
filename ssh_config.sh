624076443eae4f05718b71ce04ff39e3cb410c22
root <root@bet.local>
2019-09-20 00:53:24 +0430

    Fix SSH Banner SSHD_Config

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#sudo touch /etc/ssh/banner.txt
 sudo cp ssh_banner.txt /etc/ssh/banner.txt
 sudo sed 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo sed 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
 sudo sed 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sudo sed '$aAllowUsers mrtshoot' /etc/ssh/sshd_config
-sudo sed 's/#Banner none/Banner /etc/ssh/banner.txt/g' /etc/ssh/sshd_config
+sudo sed 's/#Banner none/Banner \/etc\/ssh\/banner.txt/g' /etc/ssh/sshd_config
 sudo mkdir /home/mrtshoot/.ssh
 sudo mkdir /home/ansible/.ssh
 sudo touch /home/mrtshoot/.ssh/authorized_keys
