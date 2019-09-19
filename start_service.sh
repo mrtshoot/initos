11310d7ff25b0946049c67b841dfdacbd3725123
Hossein Ghorbani <hosi.ghorbani@gmail.com>
2019-09-19 23:47:48 +0430

    Change Install_Package for Docker,Change Start_Service for Docker Service

--- start_service.sh
+++ start_service.sh
@@ -5,4 +5,6 @@ sudo systemctl enable ntpd &&\
 sudo systemctl restart ntpd &&\
 sudo systemctl enable sshd
 sudo systemctl restart sshd
+sudo systemctl enable docker
+sudo systemctl restart docker
 exit 0
