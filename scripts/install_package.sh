aa1801afa0dc28ec7f759f17674ea9c6e4cd71b1
Hossein Ghorbani <hosi.ghorbani@gmail.com>
2019-12-24 11:01:36 +0330

    Add expect installation package on install package script file and change users pass

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -5,6 +5,7 @@ sudo yum install yum-utils -y &&\
 sudo yum groupinstall development -y &&\
 sudo yum install https://centos7.iuscommunity.org/ius-release.rpm -y &&\
 sudo yum install python36u -y &&\
+sudo yum install expect -y &&\
 sudo yum install python36u-pip -y  &&\
 sudo yum install python36u-devel -y &&\
 sudo pip3.6 install --upgrade pip &&\
